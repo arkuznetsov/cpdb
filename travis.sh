9da6a54e9a249bad216f83a1c377ba259e7131c2
ArKuznetsov <ArKuznetsov@gmail.com>
2018-05-12 13:11:40 +0300

    Добавлены настройки сборки на travis и проверки в Sonar

---
+++ travis.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+set -e
+
+#sudo opm run coverage
+
+temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`
+version=${temp##*|}
+
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    sonar-scanner \
+        -Dsonar.host.url=https://sonar.silverbulleters.org \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.scanner.skip=false \
+        -Dsonar.branch.name=
+
+  elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    sonar-scanner \
+        -Dsonar.host.url=https://sonar.silverbulleters.org \
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.projectVersion=$version\
+        -Dsonar.scanner.skip=false \
+        -Dsonar.branch.name=
+  fi
+fi
