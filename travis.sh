c92e08fbf48c863b6f7ace3b4b3f3da8b0ce7892
AKuznetsov <arkuznetsov@gmail.com>
2019-10-03 14:32:37 +0300

    test: Изменены настройки тестов и sonar

--- travis.sh
+++ travis.sh
@@ -10,21 +10,19 @@ version=${temp##*|}
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     sonar-scanner \
-        -Dsonar.host.url=https://sonar.silverbulleters.org \
+        -Dsonar.host.url=$SONAR_HOST \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
         -Dsonar.login=$SONAR_TOKEN \
-        -Dsonar.scanner.skip=false \
-        -Dsonar.branch.name=
+        -Dsonar.scanner.skip=false
 
   elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     sonar-scanner \
-        -Dsonar.host.url=https://sonar.silverbulleters.org \
+        -Dsonar.host.url=$SONAR_HOST \
         -Dsonar.login=$SONAR_TOKEN \
         -Dsonar.projectVersion=$version\
-        -Dsonar.scanner.skip=false \
-        -Dsonar.branch.name=
+        -Dsonar.scanner.skip=false
   fi
 fi
